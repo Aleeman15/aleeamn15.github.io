f610b0c05c2e41211fd67778bf776917e9eb9ce6
Aleeman15 <aliyuabubakar152005@gmail.com>
2024-11-26 10:59:35 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/images.png") {
+    myImage.setAttribute("src", "images/Blueimages.jpeg");
+  } else {
+    myImage.setAttribute("src", "images/images.png");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `books are cool, ${myName}`;
+    }
+  }  
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `books are cool, ${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
+  
